649862de2adc9bc180b14f268786b3142bbffbbd
克洛斯 <235437881+CrossAlbert@users.noreply.github.com>
2025-11-15 19:38:05 +0800

    去除多余的屏蔽刷新快捷键语句

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, globalShortcut, ipcMain, Tray, Menu } from 'electron';
+import { app, BrowserWindow, ipcMain, Tray, Menu } from 'electron';
 import icon from '../../resources/icon.png?asset';
 import { join, normalize, resolve } from 'path';
 import fs from 'fs';
